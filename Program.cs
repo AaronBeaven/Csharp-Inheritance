cfe1ce64452b69c9b2c996bd41a24a0092a31914
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-13 11:02:43 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Csharp_Inheritance
+{
+    class HeavenWithAB
+    {
+        public void theWidth(int myWidth)
+        {
+            width = myWidth;
+        }
+        public void theHeight(int myHeight)
+        {
+            height = myHeight;
+        }
+        protected int width;
+        protected int height;
+    }
+
+    class Square : HeavenWithAB
+    // subclass created here
+    {
+        public int area(){
+            return(width*height);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating a new instance of square : mySquare
+            Square mySquare = new Square();
+            // 
+            mySquare.theHeight(10);
+            mySquare.theWidth(10);
+            Console.WriteLine("my square has a value of : {0}", mySquare.area());
+            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
